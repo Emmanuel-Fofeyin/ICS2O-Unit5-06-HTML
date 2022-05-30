5a1702b01ab388667fa575b6f3515c393a051c0c
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-30 00:28:12 +0000

    1 modified file or folder

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // This file contains the JS functions for index.html
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS20-Unit5-06-HTML/sw.js", {
